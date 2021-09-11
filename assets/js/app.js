d7ee78835dc34ebd6a68792e01e64d5bfbc137a6
Mehul Srivastava <e09885mehul@dpsrkp.net>
2021-09-11 17:40:14 +0530

    initial commit

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector("button.mobile-menu-button");
-const menu = document.querySelector(".mobile-menu");
-
-btn.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-});
--- dashboard/send_otp.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'assets/includes/functions.php';
-$phone = $_POST['phone'];
-$otp = rand(111111, 999999);
-sendOTP($otp, $phone);
-?>
